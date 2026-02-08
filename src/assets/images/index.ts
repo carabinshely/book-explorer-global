4f811895f8b9313294d59fd8af8e100e9cf4e4a2
Michael Broner <crab2007@gmail.com>
2026-02-08 14:59:34 +0200

    -
    Signed-off-by: Michael Broner <crab2007@gmail.com>

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,19 +1,31 @@
+import theLostUmbrellaOfNiranEnCover from './the-lost-umbrella-of-niran-en/cover.jpg';
+import theLostUmbrellaOfNiranEoCover from './the-lost-umbrella-of-niran-eo/cover.jpg';
 import theLostUmbrellaOfNiranCover from './the-lost-umbrella-of-niran/cover.jpg';
+import theLostUmbrellaOfNiranRuCover from './the-lost-umbrella-of-niran-ru/cover.jpg';
+import theLostUmbrellaOfNiranEn0 from './the-lost-umbrella-of-niran-en/cover.jpg';
+import theLostUmbrellaOfNiranEo0 from './the-lost-umbrella-of-niran-eo/cover.jpg';
+import theLostUmbrellaOfNiranRu0 from './the-lost-umbrella-of-niran-ru/cover.jpg';
 
 export const bookCovers: Record<string, string> = {
-  'the-lost-umbrella-of-niran-en': theLostUmbrellaOfNiranCover,
-  'the-lost-umbrella-of-niran-eo': theLostUmbrellaOfNiranCover,
+  'the-lost-umbrella-of-niran-en': theLostUmbrellaOfNiranEnCover,
+  'the-lost-umbrella-of-niran-eo': theLostUmbrellaOfNiranEoCover,
   'the-lost-umbrella-of-niran-eo-en': theLostUmbrellaOfNiranCover,
-  'the-lost-umbrella-of-niran-ru': theLostUmbrellaOfNiranCover,
+  'the-lost-umbrella-of-niran-ru': theLostUmbrellaOfNiranRuCover,
   'the-lost-umbrella-of-niran-ru-en': theLostUmbrellaOfNiranCover,
   'the-lost-umbrella-of-niran-ru-eo': theLostUmbrellaOfNiranCover,
 };
 
 export const galleryImages: Record<string, string[]> = {
-  'the-lost-umbrella-of-niran-en': [],
-  'the-lost-umbrella-of-niran-eo': [],
+  'the-lost-umbrella-of-niran-en': [
+    theLostUmbrellaOfNiranEn0,
+  ],
+  'the-lost-umbrella-of-niran-eo': [
+    theLostUmbrellaOfNiranEo0,
+  ],
   'the-lost-umbrella-of-niran-eo-en': [],
-  'the-lost-umbrella-of-niran-ru': [],
+  'the-lost-umbrella-of-niran-ru': [
+    theLostUmbrellaOfNiranRu0,
+  ],
   'the-lost-umbrella-of-niran-ru-en': [],
   'the-lost-umbrella-of-niran-ru-eo': [],
 };
